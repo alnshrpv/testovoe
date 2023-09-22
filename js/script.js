76a45e50de1f5e3359db5ce996d9bc747590c5e1
alnshrpv <124539305+alnshrpv@users.noreply.github.com>
2023-09-23 01:19:28 +0800

    izm

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
-const searchInput = document.getElementById("searchInput");
-const searchButton = document.getElementById("searchButton");
-const matchCount = document.getElementById("matchCount");
-
-searchButton.addEventListener("click", function() {
-  const searchValue = searchInput.value.toLowerCase();
-  const cells = document.querySelectorAll(".cell");
-
-  let matchCounter = 0;
-
-  cells.forEach(cell => {
-    const cellText = cell.textContent.toLowerCase();
-    if (cellText.includes(searchValue)) {
-      cell.classList.add("matched");
-      matchCounter++;
+function performSearch() {
+  let input = document.getElementById("search");
+  let filter = input.value.toLowerCase();
+  let cells = document.getElementsByClassName("cell");
+  let count = 0;
+  
+  for (let i = 0; i < cells.length; i++) {
+    let cell = cells[i];
+    let text = cell.textContent.toLowerCase();
+    
+    if (text.includes(filter)) {
+      cell.classList.add("highlight");
+      count++;
     } else {
-      cell.classList.remove("matched");
+      cell.classList.remove("highlight");
     }
-  });
-
-  matchCount.textContent = matchCounter > 0 ? `Найдено совпадений: ${matchCounter}` : "Ничего не найдено";
-});
+  }
+  
+  let searchCount = document.getElementById("searchCount");
+  
+  if (count > 0) {
+    searchCount.textContent = "Найдено совпадений: " + count;
+  } else {
+    searchCount.textContent = "Ничего не найдено";
+  }
+}
